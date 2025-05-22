b8c6f637a5667013353ded9699cae0fa6cb36a50
Ariel Subia <arielmsubia@gmail.com>
2025-05-22 13:51:59 -0300

    Add product and division functions

--- calculadora.py
+++ calculadora.py
@@ -6,5 +6,21 @@ def restar(a, b):
     """Función que resta dos números"""
     return a - b
 
+def multiplicar(a, b):
+    """Función que multiplica dos números"""
+    return a * b
+
+def dividir(a, b):
+    """Función que divide dos números"""
+    if b==0:
+        print("La variable b no puede ser cero")
+    else:
+        return a / b
+
+
+
 resultado1=sumar(5,3)
 print(resultado1)
+
+resultado2=restar(5,3)
+print(resultado2)
